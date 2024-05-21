ae2bd68b7160cb88c3e22dd9b20cf6276e578358
田頭龍星 <tagashiraryusei@tagashiraryuuseinoMacBook-Air.local>
2024-05-21 13:59:28 +0900

    study-type

---
+++ src/array.ts
@@ -0,0 +1,11 @@
+export {};
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+let number2: Array<number> = [1, 2, 3, 4, 5, 6, 7];
+let string2: Array<string> = ['tokyo', 'osaka', 'nara'];
+let string3: string[] = ['tokyo', 'osaka', 'nara'];
+let nijigenhairetsu: number[][] = [[50, 10], [50, 10], [50, 10], [50, 10]];
+let hairetsu: (string | number | boolean)[] = [1, 'tokyo', false]
+
+console.log(nijigenhairetsu);
+console.log(hairetsu);

a1b7ac9f389a86e65613b719264420b73a2e5c1d
田頭龍星 <tagashiraryusei@tagashiraryuuseinoMacBook-Air.local>
2024-05-23 19:17:14 +0900

    object

---
+++ src/object.ts
@@ -0,0 +1,8 @@
+export {};
+
+let profile: object = { name: 'Ham'};
+profile = { brrithYear: 1976};
+
+let profile2: {name: string} = {name: 'jame'};
+// profile2 = {birthYear: 1987};
+profile2 = {name: 'nimo'};

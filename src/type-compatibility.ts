400eece86d2248d0a5a8beddeb3f92bcc3624e45
田頭龍星 <tagashiraryusei@tagashiraryuuseinoMacBook-Air.local>
2024-05-26 17:03:17 +0900

    type-compatibility

---
+++ src/type-compatibility.ts
@@ -0,0 +1,29 @@
+export {};
+
+let fooCompatible: any;
+let barCompatible: string = 'TypeScript'
+
+console.log(typeof fooCompatible)
+fooCompatible = barCompatible;
+console.log(typeof fooCompatible)
+let fooIncompatible: string;
+let barIncompatible: number = 1;
+
+// fooIncompatible = barIncompatible;
+
+let fooNumber: number;
+let fooNumberLiteral: 1976 = 1976;
+
+fooNumber = fooNumberLiteral;
+
+interface Animal {
+  age: number;
+  name: string;
+}
+
+class Person {
+  constructor(public age: number, public name: string){}
+}
+
+let me: Animal;
+me = new Person(43, 'hamson');

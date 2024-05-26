efd98e2275a401d9ba5548d44ff2a3bf98e2030c
田頭龍星 <tagashiraryusei@tagashiraryuuseinoMacBook-Air.local>
2024-05-26 17:24:57 +0900

    as-const

---
+++ src/as-const.ts
@@ -0,0 +1,14 @@
+export {};
+
+let name = 'Ryusei';
+name = 'ham'
+
+let nickName = 'Ham' as const;
+// nickName = 'Hamtaro'
+let profile = {
+  name: 'Astushi',
+  height: 178
+} as const;
+
+// profile.name = 'ham';
+// profile.height = 177;
